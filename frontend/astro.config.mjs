9927e15adc5a6f817d732fed3a98c19defb71a04
IvanStrahija <strahija9999@gmail.com>
2026-09-18 22:21:56 +0200

    fix: pages base

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://kvizazist.github.io',
-  base: '/necklace/',
+  site: 'https://IvanStrahija.github.io',
+  base: '/NecklaceShowcase/',
   trailingSlash: 'always',
   build: {
     format: 'directory',
